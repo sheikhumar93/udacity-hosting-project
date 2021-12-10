49cfe046245e209d69b551d2a38525b6f1446172
Muhammad Umar <umar.sheikh.muhammad@gmail.com>
2021-12-10 18:22:46 +0500

    updated package.json

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagramapi-env.eba-pamsxsxw.us-east-1.elasticbeanstalk.com/",
+  apiHost: "http://udagramapi-env.eba-pamsxsxw.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
